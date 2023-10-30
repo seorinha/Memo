249cefc15a30e6ebb2fed3a1f4d6ef911f191019
학원 <testtest@gmail.com>
2023-10-30 21:43:23 +0900

    10/30

--- src/main/java/com/memo/user/bo/UserBO.java
+++ src/main/java/com/memo/user/bo/UserBO.java
@@ -1,18 +1,37 @@
 package com.memo.user.bo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.memo.user.entity.UserEntity;
+import com.memo.user.repository.UserRepository;
 
 @Service
 public class UserBO {
 
+	@Autowired
+	private UserRepository userRepository; 
+	
 	//input:loginId
 	//output:UserEntity(null이거나 채워져있거나 둘중의 하나의 상태로 return)
 	public UserEntity getUserEntityByLoginId(String loginId) {
-		return null;
+		return userRepository.findByLoginId(loginId);
 	}
 	
+	//input: 4개 파라미터
+	//output: id(pk)
+	public Integer addUser(String loginId, String password, String name, String email) {
+		// UserEntity = save(UserEntity);
+		UserEntity userEntity = userRepository.save(
+				UserEntity.builder()
+				.loginId(loginId)
+				.password(password)
+				.name(name)
+				.email(email)
+				.build());
+		
+		return userEntity ==  null ? null : userEntity.getId();
+	}
 	
 	
 }
---
+++ src/main/java/com/memo/user/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.memo.user.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.memo.user.entity.UserEntity;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+
+	//userentity는 null이거나 채워져있거나
+	public UserEntity findByLoginId(String loginId);
+}
